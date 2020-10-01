eee64fa2f22ff7f1535a6ac667fbcd8bf5da416d
Rene van Pelt <you@example.com>
2020-10-02 01:00:26 +0200

    fix again

--- lib/choo.rb
+++ lib/choo.rb
@@ -10,7 +10,7 @@ puts "#-----"
 EventRecord::Base.establish_connection(adapter: 'sqlite3', database: 'THISNEEDSTOCHANGE.db')
 AggregateRecord::Base.establish_connection(adapter: 'sqlite3', database: 'THISNEEDSTOCHANGE_2.db')
 puts "#-d----"
-puts Gem.loaded_specs["choochoo"].gem_dir
+puts File.read("#{Gem.loaded_specs['choochoo'].gem_dir}/admin/layout.html.haml")
 
 
 require 'choo/aggregate'
