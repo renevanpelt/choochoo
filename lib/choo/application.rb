12f8bd8ab112817b28b36ce6d7a93b1534747248
Rene van Pelt <you@example.com>
2020-10-02 03:11:36 +0200

    sorry for this, explain me how to test a gem like this without pushing it easily

--- lib/choo/application.rb
+++ lib/choo/application.rb
@@ -5,45 +5,58 @@ module Choo
 
 
 
-      @@resources = []
+    @@resources = []
 
 
-      # We load the user defined helper classes 
-      # from ./lib in the application folder
+    # We load the user defined helper classes 
+    # from ./lib in the application folder
 
-      Dir.glob("./lib/*/") do |f|
-        require f
+    Dir.glob("./lib/*/") do |f|
+      require f
+    end
+
+
+
+    # The folders in ./resources/ let the framework know 
+    # about user defined resources (models)
+
+    Dir.glob("./resources/*/") do |f|
+      resource_name = f.split("/").last
+
+      # Load the model file (AggregateRecord)
+
+      require "./resources/#{resource_name}/#{resource_name.singularize}.rb"
+      
+      @@resources << resource_name.to_sym
+      
+      # Load the commands associated with this resource
+
+      Dir.glob("./resources/#{resource_name}/commands/*.rb") do |command_file|
+        require command_file
+      end
+      
+      # Load the events associated with this resource
+      
+      Dir.glob("./resources/#{resource_name}/events/*.rb") do |event_file|
+        require event_file
       end
 
+    end
 
+    def self.routes
+      routes = Yaml.load(File.open("./config/routes.yml"))['routes']
+      returnvalue = []
 
-      # The folders in ./resources/ let the framework know 
-      # about user defined resources (models)
-
-      Dir.glob("./resources/*/") do |f|
-        resource_name = f.split("/").last
-
-        # Load the model file (AggregateRecord)
-
-        require "./resources/#{resource_name}/#{resource_name.singularize}.rb"
-        
-        @@resources << resource_name.to_sym
-        
-        # Load the commands associated with this resource
-
-        Dir.glob("./resources/#{resource_name}/commands/*.rb") do |command_file|
-          require command_file
+      routes.each do |r|
+        if r['get'].present?
+          returnvalue << {method: 'get', response: r['response']}
         end
-        
-        # Load the events associated with this resource
-        
-        Dir.glob("./resources/#{resource_name}/events/*.rb") do |event_file|
-          require event_file
-        end
+        if r['post'].present?
 
+        end
       end
 
-
+    end
 
     def self.resources
       @@resources
--- lib/choo/routing_controller.rb
+++ lib/choo/routing_controller.rb
@@ -4,11 +4,21 @@
 # be expanded with CommandThatInitiatesMultipleEvents
 # Bij wijze van spreken.
 require 'sinatra/base'
+require 'yaml'
 module Choo
   class RoutingController < Sinatra::Base
     get '/' do
       "Hello world"
     end
-     
+    routes = Yaml
+    
+    Choo::Application.routes.each do |route|
+      if routes[:method] == 'get'
+
+      elsif routes[:method] == 'post'
+
+      end
+    end
+
   end
 end
