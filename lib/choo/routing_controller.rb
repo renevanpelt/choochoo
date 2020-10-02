66297f3b25de08942e50d9042cda9f54daf70c2b
Rene van Pelt <you@example.com>
2020-10-02 02:29:52 +0200

    sorry for this, explain me how to test a gem like this without pushing it easily

--- lib/choo/routing_controller.rb
+++ lib/choo/routing_controller.rb
@@ -3,6 +3,7 @@
 # Every command inherits from BaseCommand. Might
 # be expanded with CommandThatInitiatesMultipleEvents
 # Bij wijze van spreken.
+require 'sinatra/base'
 module Choo
   class RoutingController < Sinatra::Base
     get '/' do
