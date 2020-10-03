f1c2766ed2a19d6d583421103fbb3200a03c24fd
Rene van Pelt <you@example.com>
2020-10-03 02:35:29 +0200

    sorry for this, explain me how to test a gem like this without pushing it easily

--- lib/choo.rb
+++ lib/choo.rb
@@ -37,7 +37,7 @@ class AdminController < Sinatra::Base
   end
 
   get "/admin/resources/:resource" do 
-    Choo::Application.render_template( "#{GEM_DIR}/views/admin/resource", {resource: params[:resource].capitalize.singularize.constantize}, "#{GEM_DIR}/views/admin/layout")
+    Choo::Application.render_template( "#{GEM_DIR}/views/admin/resource", {resource: params[:resource]}, "#{GEM_DIR}/views/admin/layout")
   end
 
 end
